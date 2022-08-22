81c3e9c4edac680a87b713b2ea40bed9b6325963
fannilin <103945012+fannilin@users.noreply.github.com>
2022-08-22 20:57:49 +0300

    Update App.js

--- App.js
+++ App.js
@@ -1,10 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button, Alert, TextInput, Image } from 'react-native';
+
 
 export default function App() {
+  const [text, setText] = useState('odottamaton virhe');
+  const buttonPressed = () => {
+    Alert.alert('kirjoitit: ' + text);
+  };
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Fannin mobiilisovellus</Text>
+   <Button onPress={buttonPressed} title = "Press me" />
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +19,13 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#ffp',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  input : {
+    width: 200,
+    borderColor: 'green',
+    borderWidth: 2
+  }
 });
